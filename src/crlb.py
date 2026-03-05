Fixed a bug with the phase - it was added twice. removed that and voilla - everything runs much quicker and smoother and i get reasonable results i.e. some theta and a CRLB for the standard deviation that is O(theta) or larger. hence we cannot conduct inference

src/crlb.py
@@ -87,8 +87,11 @@ def compute_crlb(S0,
     J = factor * G.T @ G
         
     # Cholesky factorization LL^T = J (L is lower triangular)
-    L = np.linalg.cholesky(J)
-    
+    try:
+        L = np.linalg.cholesky(J)
+    except LinAlgError as e:
+        print(str(e))
+        return np.inf
     # e1 = [1,0,0,0,0] since theta is first in JACOBIAN_COLS
     e1 = np.zeros(J.shape[0])
     e1[0] = 1

src/data_loader.py
@@ -80,10 +80,9 @@ def load_real(disease, regions, seasons, mortality_path="data/pni_mortality/outp
 
     # Compute phases
     phase = np.array([estimate_phase(r, humidity_dir) for r in regions])
-    mean_phase = np.mean(phase)
 
     # Convert date to continuous time with Nov 1 = Y.0
-    df["t"] = calc_t(df["date"]) + mean_phase / (2 * np.pi)
+    df["t"] = calc_t(df["date"])
     df["season"] = np.floor(df["t"]).astype(int)
     df = df[df["season"].isin(seasons)]
 